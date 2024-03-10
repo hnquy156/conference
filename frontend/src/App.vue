2dad7de26fefca942e9e317401b1a51b12ab4bd2
hnquy156 <hnquy.hbt@gmail.com>
2024-03-10 22:23:54 +0700

    Init vue3x

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  header {
+    max-height: 64px;
+    font-size: 20px;
+  }
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Conference</h1>
+    List of Conference
+  </main>
+</template>
